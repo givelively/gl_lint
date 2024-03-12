e2f5284e32ea2cad58c21ffb740a6d92fc53f79d
Seth Herr <seth.william.herr@gmail.com>
2024-03-12 15:11:27 -0700

    Update rubocop version

--- spec/gl_lint/export_rubocop_spec.rb
+++ spec/gl_lint/export_rubocop_spec.rb
@@ -57,7 +57,7 @@ RSpec.describe GlLint::ExportRubocop do
   end
 
   describe 'rubocop_version' do
-    let(:target) { '1.62.0 (using Parser 3.3.0.5, rubocop-ast 1.31.1, running on ruby 3.2.3)' }
+    let(:target) { '1.62.1 (using Parser 3.3.0.5, rubocop-ast 1.31.2, running on ruby 3.2.3)' }
 
     it 'returns the target', skip: !RubyV::ON_TARGET_VERSION do
       expect(described_class.send(:rubocop_version)).to eq target
