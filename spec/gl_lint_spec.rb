617dbc73e4ef6ec18677c9ed7610b5fbf7040744
Seth Herr <seth.william.herr@gmail.com>
2024-03-12 12:37:02 -0700

    Lint

--- spec/gl_lint_spec.rb
+++ spec/gl_lint_spec.rb
@@ -11,20 +11,27 @@ RSpec.describe GlLint do
 
     after { ARGV.replace(@original_argv) }
 
-    let(:passed_args) { ["--list-files", "--unsafe-fix"] }
+    let(:passed_args) { ['--list-files', '--unsafe-fix'] }
+
     it 'parses the options' do
-      expect_any_instance_of(GlLint::Linter).to receive(:lint).with(linters: %w[rubocop prettier], target_files: '--changed', filenames: nil, lint_strategy: :list_only)
+      expect_any_instance_of(GlLint::Linter).to receive(:lint).with(linters: %w[rubocop prettier],
+                                                                    target_files: '--changed', filenames: nil, lint_strategy: :list_only)
       described_class.call_cli(app_root:)
     end
+
     context 'with a filename' do
-      let(:passed_args) { ["--no-fix", "spec/gl_lint_spec.rb"] }
+      let(:passed_args) { ['--no-fix', 'spec/gl_lint_spec.rb'] }
+
       it 'parses the options' do
-        expect_any_instance_of(GlLint::Linter).to receive(:lint).with(linters: %w[rubocop prettier], target_files: '--changed', filenames: ['spec/gl_lint_spec.rb'], lint_strategy: :no_fix)
+        expect_any_instance_of(GlLint::Linter).to receive(:lint).with(linters: %w[rubocop prettier],
+                                                                      target_files: '--changed', filenames: ['spec/gl_lint_spec.rb'], lint_strategy: :no_fix)
         described_class.call_cli(app_root:)
       end
     end
+
     context 'with write-rubocop-rules' do
-      let(:passed_args) { ["--no-fix", "--write-rubocop-rules"] }
+      let(:passed_args) { ['--no-fix', '--write-rubocop-rules'] }
+
       it 'parses the options' do
         expect(GlLint::ExportRubocop).to receive(:write_rules).with(app_root)
         described_class.call_cli(app_root:)
