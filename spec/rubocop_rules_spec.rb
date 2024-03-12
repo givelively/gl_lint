63be8258a210193febcbef745b8c21e6c65d8a99
Seth Herr <seth.william.herr@gmail.com>
2024-03-12 12:34:14 -0700

    Add rubocop_rules_spec.rb

---
+++ spec/rubocop_rules_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe '.rubocop_rules.yml' do # rubocop:disable RSpec/DescribeClass
+  it 'has the rules' do
+    require 'gl_lint'
+
+    # Write rubocop_rules.yml
+    GlLint.call(app_root: Dir.pwd, write_rubocop_rules: true)
+
+    # Verify that the file hasn't changed
+    expect(`git diff --name-only --exit-code .rubocop_rules.yml`).to be_blank
+  end
+end
