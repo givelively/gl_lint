529443dd926a547b4e2fe0b1cfac394e394a06a9
Seth Herr <seth.william.herr@gmail.com>
2025-10-30 14:56:09 -0700

    Lint erb files

--- lib/gl_lint/file_selector.rb
+++ lib/gl_lint/file_selector.rb
@@ -1,6 +1,6 @@
 module GLLint
   class FileSelector
-    NON_RB_RUBY_FILES = %w[Gemfile Rakefile config.ru
+    NON_RB_RUBY_FILES = %w[Gemfile Rakefile config.ru .erb
                            bin/bundle bin/lint bin/rubocop bin/setup bin/update].freeze
 
     class << self
